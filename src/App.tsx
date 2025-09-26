21ae73757952bfc4ea9c759f3606e5256831e329
Hector <baseballhector@gmail.com>
2025-09-26 16:51:46 -0400

    fix: removed placeholder text

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,5 @@
 function App() {
-  return (
-    <div>
-      <p>The quick brown fox jumps over the lazy dog</p>
-      <p className="font-primary">
-        The quick brown fox jumps over the lazy dog
-      </p>
-      <p className="font-secondary">
-        The quick brown fox jumps over the lazy dog
-      </p>
-      <p className="font-secondary-italic font-semibold text-8xl">
-        The quick brown fox jumps over the lazy dog
-      </p>
-      <p className="font-fun font-semibold text-8xl bg-blue-700">
-        The quick brown fox jumps over the lazy dog
-      </p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
