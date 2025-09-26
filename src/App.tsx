fca779ef3094ca59e03deee21f62a7308199b762
Hector <baseballhector@gmail.com>
2025-09-26 16:46:29 -0400

    feat #37: adds custom colors to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <p>The quick brown fox jumps over the lazy dog</p>
+      <p className="font-primary">
+        The quick brown fox jumps over the lazy dog
+      </p>
+      <p className="font-secondary">
+        The quick brown fox jumps over the lazy dog
+      </p>
+      <p className="font-secondary-italic font-semibold text-8xl">
+        The quick brown fox jumps over the lazy dog
+      </p>
+      <p className="font-fun font-semibold text-8xl bg-blue-700">
+        The quick brown fox jumps over the lazy dog
+      </p>
+    </div>
+  );
 }
 
 export default App;
