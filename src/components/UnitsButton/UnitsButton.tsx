dd616ca92b62b5c36113c9b09f158e690254e1ea
Hector <baseballhector@gmail.com>
2025-09-26 22:46:05 -0400

    style: adjusted text styles

--- src/components/UnitsButton/UnitsButton.tsx
+++ src/components/UnitsButton/UnitsButton.tsx
@@ -4,9 +4,7 @@ const UnitsButton = () => {
   return (
     <button className="flex flex-row items-center gap-[10px] bg-neutral-800 hover:bg-neutral-600 rounded-lg px-4 py-3 cursor-pointer">
       <CogIcon />
-      <p className="font-secondary text-base text-neutral-0 leading-[120%]">
-        Units
-      </p>
+      <p className="text-preset-7 text-neutral-0">Units</p>
       <ArrowIcon />
     </button>
   );
