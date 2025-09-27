fd726e026df0347e50d5e977dc412301e48d01f6
Hector <baseballhector@gmail.com>
2025-09-27 15:24:54 -0400

    fix: adds shrink-0 to prevent logo from shrinking on page width resize

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import { LogoIcon } from "../../assets/icons/pageIcons";
 
 const Logo = () => {
   return (
-    <div className="flex flex-row justify-start items-center gap-[10px]">
+    <div className="flex shrink-0 flex-row justify-start items-center gap-[10px]">
       <LogoIcon />
       <div className="font-primary text-neutral-0 font-bold text-[22px]">
         Weather Now
