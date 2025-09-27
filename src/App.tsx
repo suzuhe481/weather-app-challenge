e6cda7d22dbc3392c57f71d1e25ddde250b0a1a0
Hector <baseballhector@gmail.com>
2025-09-27 15:55:15 -0400

    feat: importing Header and Hero components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="bg-neutral-900 pt-6 pb-10">
+      <Header />
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
