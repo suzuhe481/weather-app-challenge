e6acea6326c2cfe02a1f7552f226aa35b76fa699
Hector <baseballhector@gmail.com>
2025-09-28 17:56:46 -0400

    feat: integrates MainWeather component into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import SearchBar from "./components/SearchBar/SearchBar";
+import MainWeather from "./components/MainWeather/MainWeather";
+import StyledWrapper from "./components/StyledWrapper/StyledWrapper";
 
 function App() {
   return (
@@ -8,6 +10,13 @@ function App() {
       <Header />
       <Hero />
       <SearchBar />
+      <StyledWrapper>
+        <div className="flex justify-center flex-col xl:flex-row gap-8 pt-8 md:pt-[42px]">
+          <div className="flex flex-row justify-center min-w-[300px]">
+            <MainWeather />
+          </div>
+        </div>
+      </StyledWrapper>
     </div>
   );
 }
