da9e082d731fd391ec4d8dc77b34c60c8e5789f5
Hector <baseballhector@gmail.com>
2025-09-29 18:16:20 -0400

    feat: integrate CurrentConditions component into app

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import Hero from "./components/Hero/Hero";
 import SearchBar from "./components/SearchBar/SearchBar";
 import MainWeather from "./components/MainWeather/MainWeather";
 import HourlyForecast from "./components/HourlyForecast/HourlyForecast";
+import CurrentConditions from "./components/CurrentConditions/CurrentConditions";
 import StyledWrapper from "./components/StyledWrapper/StyledWrapper";
 
 function App() {
   return (
-    <div className="bg-neutral-900 pt-6 pb-10">
+    <div className="bg-neutral-900 pt-6 pb-10 overflow-hidden">
       <Header />
       <Hero />
       <SearchBar />
       <StyledWrapper>
         <div className="flex justify-center flex-col xl:flex-row gap-8 pt-8 md:pt-[42px]">
-          <div className="flex flex-row justify-center min-w-[300px]">
+          <div className="flex flex-col justify-start w-full xl:max-w-[800px]">
             <MainWeather />
+            <CurrentConditions />
           </div>
           <HourlyForecast />
         </div>
