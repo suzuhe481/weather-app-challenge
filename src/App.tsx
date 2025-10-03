a107234123d13ca4d65e24097b1619dc308e5b00
Hector <baseballhector@gmail.com>
2025-10-02 21:44:55 -0400

    feat: adding weather context into app

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,28 @@ import DailyForecast from "./components/DailyForecast/DailyForecast";
 import StyledWrapper from "./components/StyledWrapper/StyledWrapper";
 
 import { SettingsProvider } from "./context/SettingsContext/SettingsProvider";
+import { WeatherProvider } from "./context/WeatherContext/WeatherProvider";
 
 function App() {
   return (
     <SettingsProvider>
-      <div className="bg-neutral-900 pt-6 pb-10 overflow-hidden min-h-screen">
-        <Header />
-        <Hero />
-        <SearchBar />
-        <StyledWrapper>
-          <div className="flex justify-center flex-col xl:flex-row gap-8 pt-8 md:pt-[42px]">
-            <div className="flex flex-col justify-start w-full xl:max-w-[800px]">
-              <MainWeather />
-              <CurrentConditions />
-              <DailyForecast />
+      <WeatherProvider>
+        <div className="bg-neutral-900 pt-6 pb-10 overflow-hidden min-h-screen">
+          <Header />
+          <Hero />
+          <SearchBar />
+          <StyledWrapper>
+            <div className="flex justify-center flex-col xl:flex-row gap-8 pt-8 md:pt-[42px]">
+              <div className="flex flex-col justify-start w-full xl:max-w-[800px]">
+                <MainWeather />
+                <CurrentConditions />
+                <DailyForecast />
+              </div>
+              <HourlyForecast />
             </div>
-            <HourlyForecast />
-          </div>
-        </StyledWrapper>
-      </div>
+          </StyledWrapper>
+        </div>
+      </WeatherProvider>
     </SettingsProvider>
   );
 }
