c943a03f000f41e12d290ae9ade4bd0806387ee9
Hector <baseballhector@gmail.com>
2025-10-04 17:31:44 -0400

    feat: adds function that transforms location display name based on if it's a city in or outside USA

--- src/components/SearchItem/SearchItem.tsx
+++ src/components/SearchItem/SearchItem.tsx
@@ -1,10 +1,11 @@
 import type { Dispatch, SetStateAction, RefObject } from "react";
 import type { ILocation } from "../../utils/fetchLocations";
+import { useWeatherContext } from "../../hooks/useWeatherContext";
 
 interface ISearchItemProps {
   searchItemRefs: RefObject<HTMLButtonElement[] | null>;
   index: number;
-  location: ILocation;
+  locationData: ILocation;
   setQuery: Dispatch<SetStateAction<string>>;
   setSearchFocused: Dispatch<SetStateAction<boolean>>;
 }
@@ -12,14 +13,42 @@ interface ISearchItemProps {
 const SearchItem = ({
   searchItemRefs,
   index,
-  location,
+  locationData,
   setQuery,
   setSearchFocused,
 }: ISearchItemProps) => {
+  const { setLocationName, setCoordinates, setCountry } = useWeatherContext();
+
+  const location = transformLocationName(locationData);
+
   const handleButtonClick = () => {
-    setQuery(`${location.name}, ${location.country}`);
+    setQuery(location);
+
+    setLocationName(location);
+    setCoordinates({
+      latitude: locationData.latitude,
+      longitude: locationData.longitude,
+    });
+    setCountry(locationData.country);
+
     setSearchFocused(false);
   };
+
+  /**
+   * Transforms the location name to a more readable format.
+   * Displays city, state for United States.
+   * Displays city, country for other locations.
+   *
+   * @param locationData The location data to be transformed.
+   * @returns The transformed location name.
+   */
+  function transformLocationName(locationData: ILocation) {
+    if (locationData.country === "United States") {
+      return `${locationData.name}, ${locationData.admin1}`;
+    }
+
+    return `${locationData.name}, ${locationData.country}`;
+  }
 
   /**
    * Assigns the given HTMLButtonElement to the corresponding index in the searchItemRefs array.
@@ -37,7 +66,7 @@ const SearchItem = ({
       onClick={handleButtonClick}
       className="text-preset-7 text-neutral-0 rounded-lg text-left hover:bg-neutral-700 hover:border-neutral-600 border-transparent border-1 py-2.5 px-2 cursor-pointer"
     >
-      {location.name}, {location.country}
+      {location}
     </button>
   );
 };
