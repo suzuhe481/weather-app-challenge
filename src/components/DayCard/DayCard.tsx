496dec6a093592b78570fcf56a9542af65c7b12c
Hector <baseballhector@gmail.com>
2025-10-04 17:20:10 -0400

    feat: converts date into weekday

--- src/components/DayCard/DayCard.tsx
+++ src/components/DayCard/DayCard.tsx
@@ -1,18 +1,25 @@
 interface IDayCardProps {
   day: string;
-  weather: string;
-  high: string;
-  low: string;
+  weatherCode: number;
+  high: number;
+  low: number;
 }
 
 const DayCard = ({ day, high, low }: IDayCardProps) => {
+  // Adds time for timezone handling and date retrieval
+  const dateObject = new Date(`${day}T00:00:00`);
+
+  const date = dateObject.toLocaleDateString("en-US", {
+    weekday: "short",
+  });
+
   return (
     <div className="flex flex-col justify-between items-center py-4 px-[10px] bg-neutral-800 border-[1px] border-neutral-600 w-28 lg:w-full h-[165px] rounded-xl">
-      <div className="text-preset-6 text-neutral-0">{day}</div>
+      <div className="text-preset-6 text-neutral-0">{date}</div>
       <div className="bg-white size-10 rounded-xl"></div>
       <div className="flex flex-row w-full justify-between">
-        <div className="text-preset-7 text-neutral-0">{high}</div>
-        <div className="text-preset-7 text-neutral-200">{low}</div>
+        <div className="text-preset-7 text-neutral-0">{Math.round(high)}</div>
+        <div className="text-preset-7 text-neutral-200">{Math.round(low)}</div>
       </div>
     </div>
   );
